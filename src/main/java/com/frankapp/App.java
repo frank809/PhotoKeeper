76acccc4be2395a912b22fc35655116cd4c46066
Liu Frank <frank.2.liu@nokia.com>
2017-03-07 16:55:46 +0800

    add command line and dependency

--- src/main/java/com/frankapp/App.java
+++ src/main/java/com/frankapp/App.java
@@ -1,5 +1,7 @@
 package com.frankapp;
 
+import static java.lang.System.exit;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,17 @@ public class App
 {
     public static void main( String[] args )
     {
+        for(String s: args){
+            System.out.println(s);
+        }
+        if(args.length <2){
+            System.out.println("Please give 2 param.");
+            exit(0);
+        }
+        String sourceDir = args[0];
+        String targetDir = args[1];
+        filesKeeper files = new filesKeeper();
+        files.runTidy(sourceDir,targetDir);
         System.out.println( "Hello World!" );
     }
 }
